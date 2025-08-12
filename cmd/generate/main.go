0249918a158e96fb50bd9a175648f88e35cc9071
toby <toby.bro@pm.me>
2025-08-12 15:08:14 +0200

    fix: g in generate

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -62,7 +62,7 @@ func main() {
 	if g == 0 {
 		ginv = 0
 	} else {
-		ginv = 1 / float32(g)
+		ginv = mutate.Gx()
 	}
 	mutateSuccessful := mutate.MutateFile(svFile, ginv, "generate", verboseLevel)
 	if !mutateSuccessful {
